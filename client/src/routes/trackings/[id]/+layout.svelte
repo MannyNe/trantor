b15857fe155951ea56674cb0e9271b0150f002b0
frectonz <fraol0912@gmail.com>
2023-06-08 18:22:06 +0300

    feat: add a "no data yet" view

--- client/src/routes/trackings/[id]/+layout.svelte
+++ client/src/routes/trackings/[id]/+layout.svelte
@@ -10,6 +10,13 @@
 
 	export let data: LayoutData;
 
+	const totalVisitors = data.tracking.visitor_count_by_weekday.reduce(
+		(acc, curr) => acc + curr.count,
+		0
+	);
+
+	const noData = totalVisitors === 0;
+
 	const weekdayToString: Record<number, string> = {
 		0: 'Sun',
 		1: 'Mon',
@@ -105,197 +112,232 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class="app">
-	<h1>Tracking data for <span>{data.tracking.name}</span></h1>
-
-	<div class="tracking-id">
-		<div>
-			<p>Tracking ID</p>
-			<h3>{$page.params.id}</h3>
-		</div>
-		<button on:click={copyTrackingId}>Copy</button>
+{#if noData}
+	<div class="no-data">
+		<img src="/illustrations/no-data.svg" alt="No data" />
+		<h1>No data yet</h1>
 	</div>
 
-	<section class="stats">
-		<div>
-			<Chart title="Sessions and Visitors Per Day" type="bar" data={sessionsAndVisitorsChartData} />
+	<style>
+		.no-data {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+			gap: 2rem;
+			height: 100%;
+		}
+
+		.no-data img {
+			width: 100%;
+			max-width: 550px;
+		}
+
+		.no-data h1 {
+			font-size: 2rem;
+			margin: 0;
+			margin-bottom: 1rem;
+			font-family: 'Press Start 2P', cursive;
+			text-align: center;
+		}
+	</style>
+{:else}
+	<div class="app">
+		<h1>Tracking data for <span>{data.tracking.name}</span></h1>
+
+		<div class="tracking-id">
+			<div>
+				<p>Tracking ID</p>
+				<h3>{$page.params.id}</h3>
+			</div>
+			<button on:click={copyTrackingId}>Copy</button>
 		</div>
-		<div>
-			<Chart
-				type="radar"
-				title="Sessions and Visitors Per Hour"
-				data={sessionsAndVisitorsByHourChartData}
-			/>
-		</div>
-		<div>
-			<Chart title="Visitors By Browser" type="doughnut" data={visitorsCountByBrowser} />
-		</div>
-		<div>
-			<Chart title="Visitors By OS" type="doughnut" data={visitorsCountByOs} />
-		</div>
-		<div>
-			<Chart title="Visitors By Device" type="doughnut" data={visitorsCountByDevice} />
-		</div>
-	</section>
 
-	<section class="tab-container">
-		<header>
-			<Tabs
-				tabs={[
-					{
-						icon: { emoji: '📈', label: 'chart icon' },
-						name: 'Stats',
-						path: `/trackings/${$page.params.id}`,
-						active: $page.url.pathname === `/trackings/${$page.params.id}`
-					},
-					{
-						icon: { emoji: '⚙️', label: 'gear icon' },
-						name: 'Settings',
-						path: `/trackings/${$page.params.id}/settings`,
-						active: $page.url.pathname === `/trackings/${$page.params.id}/settings`
-					}
-				]}
-			/>
-		</header>
+		<section class="stats">
+			<div>
+				<Chart
+					title="Sessions and Visitors Per Day"
+					type="bar"
+					data={sessionsAndVisitorsChartData}
+				/>
+			</div>
+			<div>
+				<Chart
+					type="radar"
+					title="Sessions and Visitors Per Hour"
+					data={sessionsAndVisitorsByHourChartData}
+				/>
+			</div>
+			<div>
+				<Chart title="Visitors By Browser" type="doughnut" data={visitorsCountByBrowser} />
+			</div>
+			<div>
+				<Chart title="Visitors By OS" type="doughnut" data={visitorsCountByOs} />
+			</div>
+			<div>
+				<Chart title="Visitors By Device" type="doughnut" data={visitorsCountByDevice} />
+			</div>
+		</section>
 
-		<main>
-			<slot />
-		</main>
-	</section>
-</div>
+		<section class="tab-container">
+			<header>
+				<Tabs
+					tabs={[
+						{
+							icon: { emoji: '📈', label: 'chart icon' },
+							name: 'Stats',
+							path: `/trackings/${$page.params.id}`,
+							active: $page.url.pathname === `/trackings/${$page.params.id}`
+						},
+						{
+							icon: { emoji: '⚙️', label: 'gear icon' },
+							name: 'Settings',
+							path: `/trackings/${$page.params.id}/settings`,
+							active: $page.url.pathname === `/trackings/${$page.params.id}/settings`
+						}
+					]}
+				/>
+			</header>
 
-<style>
-	.app {
-		width: 80vw;
-		margin: 1rem auto;
-	}
+			<main>
+				<slot />
+			</main>
+		</section>
+	</div>
 
-	.app h1 {
-		font-size: 2rem;
-		margin-bottom: 2rem;
-		font-family: 'Press Start 2P', cursive;
-		line-height: 3rem;
-	}
-
-	.app h1 span {
-		padding: 0.5rem 1rem;
-		background-color: black;
-		color: white;
-	}
-
-	.tracking-id {
-		background-color: black;
-		color: white;
-
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-
-		width: fit-content;
-
-		margin-bottom: 2rem;
-		border: 1px solid #000;
-		box-shadow: 5px 6px rgba(0, 0, 0, 0.5);
-	}
-
-	.tracking-id div {
-		padding: 0.5rem 1rem;
-	}
-
-	.tracking-id h3 {
-		font-size: 1.5rem;
-		font-family: 'Press Start 2P', cursive;
-	}
-
-	.tracking-id p {
-		font-family: monospace;
-		margin-bottom: 10px;
-	}
-
-	.tracking-id button {
-		height: 100%;
-		font-family: monospace;
-		padding: 1.4rem 1rem;
-		background-color: blueviolet;
-		border: none;
-		color: white;
-		text-transform: uppercase;
-		border-left: 1px solid #fff;
-
-		font-size: 1rem;
-		font-weight: bold;
-		cursor: pointer;
-	}
-
-	.tracking-id button:hover,
-	.tracking-id button:focus {
-		background-color: #fff;
-		color: blueviolet;
-	}
-
-	.tracking-id button:active {
-		transform: scale(0.9);
-	}
-
-	.stats {
-		padding: 1rem;
-		display: flex;
-		overflow-x: scroll;
-		border: 2px solid #000;
-		box-shadow: 5px 6px rgba(0, 0, 0, 0.5);
-	}
-
-	/* style stats scroll bar */
-	.stats::-webkit-scrollbar {
-		height: 5px;
-	}
-	.stats::-webkit-scrollbar-track {
-		background: transparent;
-	}
-	.stats::-webkit-scrollbar-thumb {
-		background: #000;
-	}
-
-	.stats > div {
-		width: 400px;
-		height: 400px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	@media (max-width: 810px) {
+	<style>
 		.app {
-			width: 90vw;
+			width: 80vw;
+			margin: 1rem auto;
 		}
 
 		.app h1 {
-			font-size: 1.5rem;
+			font-size: 2rem;
+			margin-bottom: 2rem;
+			font-family: 'Press Start 2P', cursive;
+			line-height: 3rem;
+		}
+
+		.app h1 span {
+			padding: 0.5rem 1rem;
+			background-color: black;
+			color: white;
+		}
+
+		.tracking-id {
+			background-color: black;
+			color: white;
+
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+
+			width: fit-content;
+
+			margin-bottom: 2rem;
+			border: 1px solid #000;
+			box-shadow: 5px 6px rgba(0, 0, 0, 0.5);
+		}
+
+		.tracking-id div {
+			padding: 0.5rem 1rem;
 		}
 
 		.tracking-id h3 {
-			font-size: 0.6rem;
+			font-size: 1.5rem;
+			font-family: 'Press Start 2P', cursive;
 		}
-	}
 
-	@media (max-width: 500px) {
-		.app {
-			width: 100vw;
-			padding: 1rem;
+		.tracking-id p {
+			font-family: monospace;
+			margin-bottom: 10px;
+		}
+
+		.tracking-id button {
+			height: 100%;
+			font-family: monospace;
+			padding: 1.4rem 1rem;
+			background-color: blueviolet;
+			border: none;
+			color: white;
+			text-transform: uppercase;
+			border-left: 1px solid #fff;
+
+			font-size: 1rem;
+			font-weight: bold;
+			cursor: pointer;
+		}
+
+		.tracking-id button:hover,
+		.tracking-id button:focus {
+			background-color: #fff;
+			color: blueviolet;
+		}
+
+		.tracking-id button:active {
+			transform: scale(0.9);
 		}
 
 		.stats {
-			padding: 0.5rem;
+			padding: 1rem;
+			display: flex;
+			overflow-x: scroll;
+			border: 2px solid #000;
+			box-shadow: 5px 6px rgba(0, 0, 0, 0.5);
 		}
-	}
 
-	.tab-container {
-		border: 2px solid #000;
-		box-shadow: 5px 6px rgba(0, 0, 0, 0.5);
-		margin-top: 2rem;
-	}
+		/* style stats scroll bar */
+		.stats::-webkit-scrollbar {
+			height: 5px;
+		}
+		.stats::-webkit-scrollbar-track {
+			background: transparent;
+		}
+		.stats::-webkit-scrollbar-thumb {
+			background: #000;
+		}
 
-	.tab-container main {
-		padding: 1rem;
-	}
-</style>
+		.stats > div {
+			width: 400px;
+			height: 400px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		@media (max-width: 810px) {
+			.app {
+				width: 90vw;
+			}
+
+			.app h1 {
+				font-size: 1.5rem;
+			}
+
+			.tracking-id h3 {
+				font-size: 0.6rem;
+			}
+		}
+
+		@media (max-width: 500px) {
+			.app {
+				width: 100vw;
+				padding: 1rem;
+			}
+
+			.stats {
+				padding: 0.5rem;
+			}
+		}
+
+		.tab-container {
+			border: 2px solid #000;
+			box-shadow: 5px 6px rgba(0, 0, 0, 0.5);
+			margin-top: 2rem;
+		}
+
+		.tab-container main {
+			padding: 1rem;
+		}
+	</style>
+{/if}
