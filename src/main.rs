88dcf46f623451b484b86138e2d9641f9aa1dae0
frectonz <fraol0912@gmail.com>
2023-06-14 20:08:38 +0300

    feat: :sparkles: implement reading config from a `toml` file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,71 @@
-use std::env;
+use std::net::SocketAddr;
 
+use color_eyre::{
+    eyre::{eyre, Context},
+    Result,
+};
+use serde::Deserialize;
 use sqlx::PgPool;
+use tokio::fs;
 use trantor::server;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let args: Vec<String> = std::env::args().collect();
+    let config_path = &args.get(1).ok_or_else(|| eyre!("Missing config path"))?;
+    let config = fs::read_to_string(config_path).await?;
+    let config: Config = toml::from_str(&config)?;
+
     tracing_subscriber::fmt()
         .with_env_filter("info")
         .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
         .init();
 
-    let db_url = env::var("DATABASE_URL")?;
-    let pool = PgPool::connect(&db_url).await?;
-    tracing::info!("Connected to {}", &db_url);
+    let pool = PgPool::connect(&config.database)
+        .await
+        .wrap_err_with(|| format!("couldn't connect to database with url: {}", config.database))?;
 
     let routes = server(pool).await?;
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    let addr: SocketAddr = config.address.parse()?;
+
+    if let Some(https) = config.https {
+        let key = fs::read(&https.key_path).await.wrap_err_with(|| {
+            format!(
+                "couldn't read https certificate key from file: {}",
+                https.key_path
+            )
+        })?;
+        let cert = fs::read(&https.cert_path).await.wrap_err_with(|| {
+            format!(
+                "couldn't read https certificate from file: {}",
+                https.cert_path
+            )
+        })?;
+
+        warp::serve(routes)
+            .tls()
+            .key(key)
+            .cert(cert)
+            .run(addr)
+            .await;
+    } else {
+        warp::serve(routes).run(addr).await;
+    }
 
     Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    address: String,
+    database: String,
+    https: Option<Https>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Https {
+    cert_path: String,
+    key_path: String,
 }
